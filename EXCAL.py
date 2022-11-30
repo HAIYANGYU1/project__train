3ec54988a4028eb7a33e80bb7583a8a065649be2
HAIYANGYU1 <yhyxx521@gmail.com>
2022-11-30 15:41:18 +0100

    excal

--- EXCAL.py
+++ EXCAL.py
@@ -3,10 +3,14 @@ import pandas as pd
 
 data = pd.read_csv('radar.csv',sep=';')
 d = data.d.tolist()
-print(type(d))
 
-# sep=';'
-# num = 0
-# for d in list_d[]:
-
-#     if d != 1000 and d != 
+num = 0
+count = 1
+for n in d:
+    if n < 1000:
+        num = num + count
+        count = 0
+    else:
+        count = 1
+    print(f'"num:"{num},"count:"{count}')
+        
